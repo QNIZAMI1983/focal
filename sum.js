bc8e8999ba5a0d6d80a67a67d0d1efb452b27657
Qais Nizami <qais.nizami@gmail.com>
2023-07-24 19:25:30 +0000

    changed sum.js

--- sum.js
+++ sum.js
@@ -1,2 +1,11 @@
-const args = process.argv;
-console.log(args);
+const args = process.argv.slice(2);
+
+let sum = 0;
+for (let i = 0; i < args.length; i++) {
+  const number = parseFloat(args[i]);
+  if (!isNaN(number)) {
+    sum += number;
+  }
+}
+
+console.log("Result:", sum);
