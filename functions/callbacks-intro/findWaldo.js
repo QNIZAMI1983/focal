81e8b20def9f804041bc6a7529ae10bbf91adb12
Qais Nizami <qais.nizami@gmail.com>
2023-07-29 06:07:10 +0000

    Exercise 2: findWaldo.js

--- functions/callbacks-intro/findWaldo.js
+++ functions/callbacks-intro/findWaldo.js
@@ -1,17 +1,13 @@
-// findWaldo.js
+const findWaldo = function (names, found) {
+    names.forEach((name, index) => {
+        if (name === "Waldo") {
+            found(index); // Pass the index to the callback
+        }
+    });
+};
 
-const findWaldo = function(names, found) {
-    for (let i = 0; i < names.length; i++) {
-      let name = names[i];
-      if (name === "Waldo") {
-        found(i); // Pass the index to the callback
-      }
-    }
-  };
-  
-  const actionWhenFound = function(index) {
+const actionWhenFound = function (index) {
     console.log(`Found Waldo at index ${index}!`);
-  };
-  
-  findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-  
+};
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
